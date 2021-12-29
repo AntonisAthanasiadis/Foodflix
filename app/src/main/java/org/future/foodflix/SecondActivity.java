157acc075ed2494d9988e796b6ff73e4642024b8
demetrees <alekopoulosd@gmail.com>
2021-12-29 20:30:24 +0200

    Added Recyclerview , API calls templates.

--- app/src/main/java/org/future/foodflix/SecondActivity.java
+++ app/src/main/java/org/future/foodflix/SecondActivity.java
@@ -1,40 +1,61 @@
 package org.future.foodflix;
 
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
+
+    String titles[], desc[];
+    int images[] = {R.drawable.food3,
+            R.drawable.food2,
+            R.drawable.food3,
+            R.drawable.food2,
+            R.drawable.food3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        getBaseContext().getResources().getString(R.string.app_name);
+
+        recyclerView = findViewById(R.id.RV1);
+
+        titles = getResources().getStringArray(R.array.Titles);
+        desc = getResources().getStringArray(R.array.Descriptions);
+
+        myAdapter = new MyAdapter(this,titles,desc,images);
+
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
 
-    @Override
-    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        //String name= getIntent().getExtras().getString("name");
-        //Log.d("APP",name);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //String name= getIntent().getExtras().getString("name");
-        //Log.d("APP",name);
-    }
-
-    @Override
-    public void onBackPressed() {
-        Toast.makeText(SecondActivity.this,"Do you want to go back?",Toast.LENGTH_SHORT).show();
-        setResult(RESULT_OK);
-        SecondActivity.this.finish();
-    }
+//    @Override
+//    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//        //String name= getIntent().getExtras().getString("name");
+//        //Log.d("APP",name);
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        //String name= getIntent().getExtras().getString("name");
+//        //Log.d("APP",name);
+//    }
+//
+//    @Override
+//    public void onBackPressed() {
+//        Toast.makeText(SecondActivity.this,"Do you want to go back?",Toast.LENGTH_SHORT).show();
+//        setResult(RESULT_OK);
+//        SecondActivity.this.finish();
+//    }
 }
