c3fa12bd9ce91e6a0c237daa388e4dd93f99d57c
Panagiotis Evagelidakis <pevagelidakis@athtech.gr>
2022-01-11 01:01:39 +0200

    RangeSlider and SwitchCompat were added in main search  activity +  dependency between RangeSlider and SwitchCompat +  url modification according to user's search +  try to make reusable code in NetWorkActivity  (Business logic in CheckActions.java)
     <!> RecyclerView after search is NOT working properly.
     We are born to debug, aren't we?

--- app/src/main/java/org/future/foodflix/CheckActions.java
+++ app/src/main/java/org/future/foodflix/CheckActions.java
@@ -4,16 +4,54 @@ import android.widget.CheckBox;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.room.Room;
 
+import com.google.android.material.slider.RangeSlider;
 import com.google.android.material.textfield.TextInputEditText;
 
 import org.future.foodflix.Storage.Database.DatabaseSchema;
 
+import java.util.List;
+
 public class CheckActions {
 
+    public static String ingrCheck(String url, String ingr, String diet,String health,String calories){
+        if (!ingr.equals("")) {
+            url += "&ingr=0-" + ingr + diet + health + calories;
+        } else if (ingr.equals("")) {
+            url += diet + health + calories;
+        }
+        return url;
+    }
 
-    public String eval(String pass1, String pass2,String newUser, String firstName, String lastName){
+    public static void switchCompatCheckedUI(SwitchCompat switchCompat,RangeSlider slider){
+        if (switchCompat.isChecked()) {
+            slider.setEnabled(true);
+        } else {
+            slider.setEnabled(false);
+        }
+    }
+
+    public static String switchCompatCheckedCalories(SwitchCompat switchCompat,RangeSlider slider, String[] SliderValues){
+        if (switchCompat.isChecked()) {
+            return "&calories="+ calories(slider, SliderValues);
+        } else {
+            return "";
+        }
+    }
+        public static String calories(RangeSlider slider, String[] s) {
+        List<Float> values = slider.getValues();
+        s[0] = "";
+        for (float f : values) {
+            s[0] += String.valueOf((int) f);
+            s[0] += "-";
+        }
+        s[0] = s[0].substring(0, s[0].length() - 1);
+        return s[0];
+    }
+
+    public String eval(String pass1, String pass2, String newUser, String firstName, String lastName){
         if (pass1.length()<6){
             return "Password is too short";
         }
@@ -31,7 +69,7 @@ public class CheckActions {
         }
 
     }
-    public static String ingredients(String string){
+    public static String queries(String string){
         String[] strings = string.split(",");
         String s="";
         for (String str:strings){
@@ -42,11 +80,11 @@ public class CheckActions {
         return s;
     }
 
-    public static String Checked(CheckBox[] checkBoxes){
+    public static String Checked(CheckBox[] checkBoxes,String string){
         String s = "";
         for (CheckBox checkBox:checkBoxes){
             if (checkBox.isChecked()){
-                s+="&health=";
+                s+="&"+string+"=";
                 s+=checkBox.getText().toString();
             }
         }
--- app/src/main/java/org/future/foodflix/Network/NetWorkActivity.java
+++ app/src/main/java/org/future/foodflix/Network/NetWorkActivity.java
@@ -7,13 +7,18 @@ import android.util.Log;
 import android.view.View;
 import android.widget.*;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.gson.Gson;
 
+import org.future.foodflix.CheckActions;
 import org.future.foodflix.Network.JsonResponse.JsonResponse;
 import org.future.foodflix.R;
 import org.future.foodflix.RecyclerView2.ListItem;
@@ -44,47 +49,12 @@ public class NetWorkActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
         gson = new Gson();
 
-        String url = "https://api.edamam.com/api/recipes/v2?type=public" +
-                "&q=chicken" +
-                "&app_id=85236044&app_key=d2448eb147c0c48689ed8a40d9c107de" +
-                "&random=true";
-
-
-        //    Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                         jsonResponse = gson.fromJson(response, JsonResponse.class);
-                        LoadRecyclerViewData(jsonResponse);
-                        //    Display the first 500 characters of the response string.
-                    }
-                }, new Response.ErrorListener() {
+        RangeSlider slider = findViewById(R.id.slider);
+        SwitchCompat switchCompat = findViewById(R.id.switchWidget);
+        switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("TAG", error.getMessage());
-            }
-        });
-
-        //   Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
-        CheckBox vegetarian = findViewById(R.id.vegetarian);
-        CheckBox vegan = findViewById(R.id.vegan);
-        CheckBox lowFat = findViewById(R.id.low_fat);
-        CheckBox pork = findViewById(R.id.pork);
-        CheckBox gluten = findViewById(R.id.gluten);
-        CheckBox immune = findViewById(R.id.immunosupportive);
-        CheckBox wheat = findViewById(R.id.wheat);
-        CheckBox shellfish = findViewById(R.id.shellfish);
-        CheckBox[] Health = {vegetarian, vegan, lowFat, pork, gluten, immune, wheat, shellfish};
-
-
-        ImageView imageView = findViewById(R.id.searchbackbtn);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                CheckActions.switchCompatCheckedUI((SwitchCompat) buttonView,slider);
             }
         });
 
@@ -92,20 +62,98 @@ public class NetWorkActivity extends AppCompatActivity {
         nwbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                CheckBox vegetarian = findViewById(R.id.vegetarian);
+                CheckBox vegan = findViewById(R.id.vegan);
+                CheckBox immune = findViewById(R.id.immunosupportive);
+                CheckBox gluten = findViewById(R.id.gluten);
+
+                CheckBox balanced = findViewById(R.id.balanced);
+                CheckBox highProtein = findViewById(R.id.high_protein);
+                CheckBox lowFat = findViewById(R.id.low_fat);
+                CheckBox lowCarb = findViewById(R.id.low_carb);
+
+                CheckBox[] Health = {vegetarian, vegan, immune, gluten};
+                CheckBox[] Diet = {balanced, highProtein, lowFat, lowCarb};
+
+                TextInputLayout ingredients = findViewById(R.id.ingrInput);
+                String ingr = ingredients.getEditText().getText().toString();
+                String diet = CheckActions.Checked(Diet, "diet");
+
+                TextInputLayout Queries = findViewById(R.id.foodInput);
+                String query = Queries.getEditText().getText().toString();
+                String q = CheckActions.queries(query);
+                String health = CheckActions.Checked(Health, "health");
+
+                RangeSlider slider = findViewById(R.id.slider);
+                SwitchCompat switchCompat = findViewById(R.id.switchWidget);
+                switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                        CheckActions.switchCompatCheckedUI((SwitchCompat) buttonView,slider);
+                    }
+                });
+                final String[] SliderValues = {""};
+                slider.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
+                    @Override
+                    public void onStartTrackingTouch(@NonNull RangeSlider slider) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(@NonNull RangeSlider slider) {
+                        SliderValues[0] += CheckActions.calories(slider, SliderValues);
+                    }
+                });
+
+                String calories = CheckActions.switchCompatCheckedCalories(switchCompat,slider,SliderValues);
+
+                String url = "https://api.edamam.com/api/recipes/v2?type=public" +
+                        "&q=" + q +
+                        "&app_id=85236044&app_key=d2448eb147c0c48689ed8a40d9c107de";
+
+                url=CheckActions.ingrCheck(url,ingr,diet,health,calories);
+
+                //    Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                jsonResponse = gson.fromJson(response, JsonResponse.class);
+                                LoadRecyclerViewData(jsonResponse);
+                                //    Display the first 500 characters of the response string.
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(NetWorkActivity.this, "An error occured:\n"+ error.getMessage(),Toast.LENGTH_LONG).show();
+
+                    }
+                });
+
+                //   Add the request to the RequestQueue.
+                queue.add(stringRequest);
+
+
+                ImageView imageView = findViewById(R.id.searchbackbtn);
+                imageView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onBackPressed();
+                    }
+                });
+//                Toast.makeText(NetWorkActivity.this,url,Toast.LENGTH_LONG).show();
+
                 Intent intent = new Intent(NetWorkActivity.this, SecondActivity.class);
-               intent.putExtra("response",listItems);
+                intent.putExtra("response", listItems);
                 startActivity(intent);
+
             }
-
         });
-
-
-        }
+    }
 
     private void LoadRecyclerViewData(JsonResponse jsonResponse) {
-//        ProgressBar progressBar = new ProgressBar(this);
-//        progressBar.set("Loading data...");
-//        progressBar.show();
+//
 
         int i ;
         for ( i=0; i < jsonResponse.getHits().size(); i++)
@@ -117,5 +165,3 @@ public class NetWorkActivity extends AppCompatActivity {
             ));
     }
 }
-
-
